34e943a1f8cf195dbe667fe1cada8385afb54aaf
AlexCourivaud <alexandre.courivaud@gmail.com>
2025-07-29 10:57:44 +0200

    MAJ arborescence de base projet memory

--- src/STMemory/settings.py
+++ src/STMemory/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(BASE_DIR,"ouiiiiiiiiiiiiiiiiiiiiiiiiiii")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # "MemoryNote"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = "STMemory.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "STMemory/template/views")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
